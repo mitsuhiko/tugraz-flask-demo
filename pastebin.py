720e2d2d138d813723086f62ac3fd947835cec0f
Armin Ronacher <armin.ronacher@active-4.com>
2011-11-23 12:42:02 +0100

    Fixed a typo

--- pastebin.py
+++ pastebin.py
@@ -108,7 +108,7 @@ def logout():
 @app.route('/login/authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
-    next_url = request.args.get('next') or url_for('index')
+    next_url = request.args.get('next') or url_for('new_paste')
     if resp is None:
         flash('You denied the login')
         return redirect(next_url)
